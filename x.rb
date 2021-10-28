e2912d4703a24db623d0d4da73797d385f4fdd5f
taj1-takum1 <takumin390511@gmail.com>
2021-10-28 17:22:21 +0900

    indentline確認

--- x.rb
+++ x.rb
@@ -2,3 +2,16 @@ a = 1
 b = 2
 
 puts 0 if a < b
+if a < b
+  puts "大きい数です"
+end
+
+if a < b
+  if a < b
+    puts "indentlineを確認"
+    puts "indentline"
+    puts "indent"
+    puts a
+    puts b
+  end
+end
